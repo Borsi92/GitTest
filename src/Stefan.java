0c6d1e430dd09e1a6d424f1907709cfc4a0a8e45
Borsi92 <stefanborsutzky@gmx.de>
2018-02-19 22:06:53 +0100

    Ausgabe mit Hallo

--- src/Stefan.java
+++ src/Stefan.java
@@ -1,7 +1,7 @@
 public class Stefan {
 
     public static void main(String[] args) {
-        System.out.println("JOJOJO " + ausgabe());
+        System.out.println("Hallo " + ausgabe());
     }
 
     private static String ausgabe(){
